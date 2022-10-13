1dbc98ba19a16607f0983b4189169d9926a8cef8
GeoffNixCodes <97037675+GeoffNixCodes@users.noreply.github.com>
2022-10-13 14:55:28 -0400

    tuples

---
+++ tuples.js
@@ -0,0 +1,10 @@
+var drink = {
+    color: 'brown',
+    carbonated: true,
+    sugar: 40
+};
+//Tuples
+var pepsi = ['brown', true, 40];
+var sprite = ['clear', true, 40];
+var tea = ['green', false, 0];
+//# sourceMappingURL=tuples.js.map
