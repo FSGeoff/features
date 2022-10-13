585340bb2be576849ec53500f1495136682fe526
GeoffNixCodes <97037675+GeoffNixCodes@users.noreply.github.com>
2022-10-13 01:28:19 -0400

    initial commit

---
+++ types.js
@@ -0,0 +1,12 @@
+var today = new Date();
+today.getTime();
+var person = {
+    age: 20
+};
+var Color = /** @class */ (function () {
+    function Color() {
+    }
+    return Color;
+}());
+var red = new Color();
+//# sourceMappingURL=types.js.map
